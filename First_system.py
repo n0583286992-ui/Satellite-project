285530c56c39dfb1315d9d957cd8bbcbc8e138e1
netanel heller <n0583286992@gmail.com>
2026-01-28 12:09:44 +0200

    level 1

---
+++ First_system.py
@@ -0,0 +1,20 @@
+from space_network_lib import SpaceEntity, SpaceNetwork,Packet
+
+network = SpaceNetwork(level=1)
+class Satellite(SpaceEntity):
+    def __init__(self, name, distance_from_earth,level=1):
+        super().__init__(name, distance_from_earth)
+        self.level = level
+
+    def receive_signal(self, packet: Packet):
+        print(f" [{self.name}]  Received: {packet}")
+
+
+str1 = Satellite("str1",100)
+str2 = Satellite("str2",200)
+
+my_packet = Packet("i am the first")
+
+my_packet.sender = str1
+my_packet.receiver = str2
+network.send(my_packet)
